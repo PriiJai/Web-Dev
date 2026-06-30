a491fed6a6721e1ddc2c3f06c929649790bebbe2
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-06-30 14:37:26 +0530

    jss

--- 10.js
+++ 10.js
@@ -121,25 +121,138 @@
 
 
 
-function createCounter(){
-  let count = 0;
-  function increment (){
-  count ++;
-   return count;
-  }
-  return increment;
-}
+// function createCounter(){
+//   let count = 0;
+//   function increment (){
+//   count ++;
+//    return count;
+//   }
+//   return increment;
+// }
 
-const counter = createCounter();
-console.log(counter());
-console.log(counter());
-console.log(counter());
-console.log(counter());
+// const counter = createCounter();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
 
-//OUTPUT : 1,2,3,4, becuase of closure
-
-
+// //OUTPUT : 1,2,3,4, becuase of closure
 
 
 
+
+//closure in bancking system
+//  let balance = 500;
+// //  balance += "Rohit";
+//  balance -=40;
+// console.log(balance);
+
+// let user = {
+//   balance : 500,
+//   deposit:function(amount){
+//     // console.log(this);
+//     if(typeof amount =="number" && amount >0){
+//     this.balance+=amount;
+//     return this.balance;
+//     }
+//   }
+
+// }
+// console.log(user.deposit(100));
+// console.log(user.deposit("100")); //error
+// console.log(user.deposit(1sd00)); //error
+
+
+
+// let user = {
+//   balance : 500,
+//   deposit:function(amount){
+//     if(typeof amount =="number" && amount >0){
+//     this.balance+=amount;
+//     return this.balance;
+//     }
+//   } ,
+
+// withdraw:function(amount){
+//     if(typeof amount =="number" && amount >0 && this.balance>=amount){
+//     this.balance+=amount;
+//     return this.balance;
+//     }
+//   },
+// getBalance: function(){
+//   return this.balance;
+// }
+// }
+
+// // user.balance="Rohit"; can get access throught his too
+// user.balance="Rohit";
+// console.log(user.getBalance());
+
+//we get direct acees 
+//only get access to method 
+//balance : they cant get the direct access of balance
+
+//use private
+
+// function createBankAccount(){
+//   let balance  = 500;
+//   return{
+  
+//   deposit:function(amount){
+//     if(typeof amount =="number" && amount >0){
+//     balance+=amount;
+//     return balance;
+//     }
+//   } ,
+
+// withdraw:function(amount){
+//     if(typeof amount =="number" && amount >0 && this.balance>=amount){
+//     balance-=amount;
+//     return balance;
+//     }
+//   },
+// getBalance: function(){
+//   return balance;
+// }
+// }
  
+// }
+
+// const customer = createBankAccount();
+// // console.log(customer);
+// console.log(customer.withdraw(200));
+// // console.log(customer.balance);//error
+
+// //now no direct access
+
+
+//  HIGHER ORDER FUNCTION
+// function double(){
+//   function execute(){
+//     console.log("hello");
+//   }
+//   return execute;
+// }
+// const n = double();
+// n();
+
+// function double(){
+//   return function execute(){
+//     console.log("hello");
+//   }
+// }
+// const n = double();
+// n();
+
+function double(value){
+  return function execute(num){
+    return num*value;
+  }
+
+}
+const n = double(20);
+const n = double(20)(5);
+// n();
+// console.log(n);
+// console.log(n);
+console.log(n(5));
