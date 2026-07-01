c8bb653bae8672be25ef9edc04d044e9ca2ae561
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-07-01 10:43:08 +0530

    JS

---
+++ JAVASCRIPT/js for web  dev/3/1.js
@@ -0,0 +1,10 @@
+//EVENTS
+//event:moves,scroll,click,double click
+//event listener :listening the event (here  click) //onclick=functionname();
+//event action : (here): Pri is here
+
+
+function handleClick(){
+  const element = document.getElementById("first");
+  element.textContent ="Pri is here";
+}
