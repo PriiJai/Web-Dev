7e541bf67776c9d5b8ab34c6eb598b919dd8cff5
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-07-04 15:57:46 +0530

    Prototype

---
+++ JAVASCRIPT/js for web  dev/8/1.js
@@ -0,0 +1,124 @@
+// // //Prototype and classes
+
+// // const obj ={
+// //   name: "Pri",
+// //   age: 20,
+// //   greet: function(){
+// //     console.log("Hello Ji")
+// //   }
+// // };
+
+// // // console.log(obj);
+// // // console.log(obj.name);
+// // // console.log(obj.age);
+// // // console.log(obj.greet);
+// // // console.log(obj.greet());
+// // console.log(obj.hasOwnProperty("name"));
+// // console.log(obj.hasOwnProperty("names")); //false as key is not defined
+// // console.log(obj.toString());
+
+
+
+// // const arr = [10,20,30];
+// // console.log(arr.length);
+
+
+// // const obj = {
+// //   account : 30
+// // }
+
+// // console.log(obj.account)
+// // console.log(obj.name) //undefined
+
+
+
+
+
+
+// const obj ={
+//   name: "Pri",
+//   age: 20,
+//   greet: function(){
+//     console.log("Hello Ji")
+//   }
+// };
+
+
+// const obj1 = {
+//   account : 30
+// }
+
+// //prototype
+// obj1.__proto__ = obj;
+// console.log(obj1.name)
+
+
+// //in incpect you can check the prototype 
+// arr.prototype
+// Object.prototype
+//arr.__proto__
+//arr.__proto__.__proto__  //array protype linked to object prototype
+//arr.toString()
+//Arrya.prototype.__prototype__ //to go to above by one step 
+//arr-> Array.prototype -> Object.prototype -> null
+//function-> Function.prototype -> Object.prototype -> null
+
+//why null??--> because that does not exist
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
