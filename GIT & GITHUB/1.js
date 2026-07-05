21a458f6919f75db0f4d2c08a449c5ff54f99821
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-07-05 13:03:45 +0530

    GIT

---
+++ GIT & GITHUB/1.js
@@ -0,0 +1,16 @@
+ const name = "Rohit"            |    
+ const age= 23;                  
+ let balance = 240;              
+                                 
+ function updatebalance(value){  
+ baclance += value;              
+  }                              
+ 
+  
+  //the below program crashes the project 
+while( true ) {
+  console.log(20);
+}
+
+//so we have to undo what we did (this is the bug )
+
