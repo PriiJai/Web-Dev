ecfe74736648072c5fed1ed6f84b9479a22b9890
Wanderer65 <priyanka06kumari0403@gmail.com>
2026-07-06 20:42:47 +0530

    Git

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
- const name = "Rohit"            |    
+ const name = "Rohit"     ;          
  const age= 23;                  
  let balance = 240;              
                                  
@@ -6,11 +6,16 @@
  baclance += value;              
   }                              
  
+
+  console.log("Hello Coders");
   
   //the below program crashes the project 
-while( true ) {
-  console.log(20);
-}
+// while( true ) {
+//   console.log(20);
+// }
 
 //so we have to undo what we did (this is the bug )
 
+//code written(in projects): By 5 members : so how will we know who wrote the code and which lines
+//updated code :
+// 3 lines of code written
